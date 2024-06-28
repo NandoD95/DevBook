4c45364ab3fdb7c5a26cd9757123a9cf07c952f7
Fernando De Jesus <fdejesus95@outlook.com>
2024-06-28 13:28:17 -0400

    completed login.js, created redirect component and added it to app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./pages/Login";
+import Redirect from "./Redirect";
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userId, setUserId] = useState(0)
+
+  useEffect(() => {
+    fetch('/api/session')
+      .then(response => {
+        if (response.ok) {
+          setIsLoggedIn(true);
+          return response.json();
+        } else {
+          setIsLoggedIn(false);
+        }
+      })
+      .then(data => {
+        if (data !== undefined) {
+          setUserId(data['id']);
+        }
+      })
+      .catch(error => {
+        console.error('Session check failed:', error);
+      });
+  }, []);
+
   return (
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Redirect isLoggedIn={isLoggedIn} userId={userId} />} />
+        <Route path='/Login' element={<Login setIsLoggedIn={setIsLoggedIn} setUserId={setUserId}/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,10 +1,72 @@
-import React from "react";
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
-function Login (){
-    return(
-        <div>
-            <h1>Login</h1>
-        </div>
+function Login({ setIsLoggedIn, setUserId }) {
+
+    const [loginUser, setLoginUser] = useState("")
+    const [loginPassword, setLoginPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    function logIn(e) {
+        e.preventDefault()
+        fetch('/api/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(
+                {
+                    username: loginUser,
+                    password: loginPassword
+                }
+            )
+        })
+            .then(r => {
+                if (r.ok) {
+                    return r.json()
+                }
+                else {
+                    alert("Not Valid Login")
+                    return undefined
+                }
+            })
+            .then(data => {
+                if (data === undefined) {
+                    alert("Something went wrong. Please try again.")
+                }
+                else {
+                    const userId = data.id
+                    setUserId(userId)
+                    setLoginPassword("")
+                    setLoginUser("")
+                    navigate(`/user/${userId}`)
+                    setIsLoggedIn(true)
+                }
+            })
+    }
+
+    return (
+        <>
+            <div className="">
+                <h1 className="">DevBook</h1>
+            </div>
+            <div className="">
+                <h2 className="">Welcome to DevBook</h2>
+                <form className="" onSubmit={(e) => logIn(e)}>
+                    <div>
+                        <h3 className="">Username:</h3>
+                        <input id="username" className="inputs" autoComplete="username" onChange={(e) => setLoginUser(e.target.value)} />
+                    </div>
+                    <div>
+                        <h3 className="">Password:</h3>
+                        <input id="password" className="inputs" autoComplete="current-password" type="password" onChange={(e) => setLoginPassword(e.target.value)} />
+                    </div>
+                    <button className="" type="submit">Log In</button>
+                </form>
+                <h3 className="">New to DevBook? <Link className="" to="/signup">Sign Up</Link></h3>
+            </div>
+        </>
     )
 }
 
