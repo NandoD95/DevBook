9254058c6ea6652364b4b708f561c5a40ba1cf15
Fernando De Jesus <fdejesus95@outlook.com>
2024-06-29 12:44:15 -0400

    added user, checksession, signup, login, logout classes and routes to app.py

--- server/app.py
+++ server/app.py
@@ -17,6 +17,69 @@ from models import User, Post, Project, Favorite
 def index():
     return '<h1>Project Server</h1>'
 
+class Users(Resource):
+    def get(self):
+        users = User.query.all()
+        users_list = [user.to_dict() for user in users]
+        return make_response(users_list, 200)
+    
+api.add_resource(Users, '/users')
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = db.session.get(User, user_id)
+            if user:
+                return make_response(user.to_dict(), 200)
+        return make_response({'error': 'Unauthorized: Must login'}, 401)
+
+api.add_resource(CheckSession, '/check_session')
+
+class SignUp(Resource):
+    def post(self):
+        params = request.json
+        username=params.get('username')
+        if User.query.filter_by(username=username).first():
+            return make_response({"error": "Username already exists"}, 401)
+        try:
+            user = User(
+                username=params.get('username'),
+                name=params.get('name'),
+                email=params.get('email')
+            )
+            user.password_hash = params.get('password')
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return make_response(user.to_dict(), 201)
+        except Exception as e:
+            return make_response({"error": str(e)}, 400)
+        
+api.add_resource(SignUp, '/signup')
+
+class Login(Resource):
+    def post(self):
+        params = request.json
+        user = User.query.filter_by(username=params.get('username')).first()
+        if not user:
+            return make_response({'error': 'Invalid user or password'}, 404)
+
+        if user.authenticate(params.get('password')):
+            session['user_id'] = user.id
+            return make_response(user.to_dict())
+        else:
+            return make_response({'error': 'Invalid user or password' }, 404)
+
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response({}, 204)
+
+api.add_resource(Logout, '/logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
